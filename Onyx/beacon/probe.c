237722e82dbb98aa4af8b10dd1fb5ff55aa8a449
Jon Watte <jwatte@gmail.com>
2013-01-21 19:47:49 -0800

    more useful options

--- Onyx/beacon/probe.c
+++ Onyx/beacon/probe.c
@@ -12,6 +12,11 @@
 
 unsigned short port = 10169;
 
+typedef enum {
+    false, true, FileNotFound   //  :-)
+} bool;
+
+
 static char const *ipaddr(struct sockaddr_in const *sin) {
 	static char ret[32];
 	unsigned char const *addr = (unsigned char const *)&sin->sin_addr;
@@ -20,19 +25,49 @@ static char const *ipaddr(struct sockaddr_in const *sin) {
 }
 
 int sock = -1;
+bool once = false;
+bool quiet = false;
+bool verbose = false;
 
-int main() {
+int main(int argc, char const *argv[]) {
+
+    while (argv[1]) {
+        if (!strcmp(argv[1], "-1")) {
+            once = true;
+        }
+        else if (!strcmp(argv[1], "-q")) {
+            quiet = true;
+            verbose = false;
+        }
+        else if (!strcmp(argv[1], "-v")) {
+            verbose = true;
+            quiet = false;
+        }
+        else {
+            fprintf(stderr, "usage: probe [-qv1]\n");
+            exit(1);
+        }
+        ++argv;
+        --argc;
+    }
 
 again:
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sock < 0) {
-		perror("socket");
+        if (!quiet) {
+            perror("socket");
+        }
 		exit(1);
 	}
+    if (verbose) {
+        fprintf(stderr, "socket %d\n", sock);
+    }
 	int one = 1;
 	int err = setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &one, sizeof(one));
 	if (err < 0) {
-		perror("sockopt broadcast");
+        if (!quiet) {
+            perror("sockopt broadcast");
+        }
 		exit(1);
 	}
     for (int i = 0; i < 10; ++i) {
@@ -42,8 +77,13 @@ again:
         memset(&sin.sin_addr, 0xff, sizeof(sin.sin_addr));
         int w = sendto(sock, "discover", 8, 0, (struct sockaddr *)&sin, sizeof(sin));
         if (w != 8) {
-            perror("sendto");
+            if (!quiet) {
+                perror("sendto");
+            }
             exit(2);
+        }
+        if (verbose) {
+            fprintf(stderr, "sendto %d\n", w);
         }
         fd_set fds;
         FD_ZERO(&fds);
@@ -52,22 +92,36 @@ again:
         tv.tv_sec = 0;
         tv.tv_usec = 100000;
         while (select(sock+1, &fds, 0, 0, &tv) > 0) {
+            if (verbose) {
+                fprintf(stderr, "select\n");
+            }
             char buf[1024];
             socklen_t slen = sizeof(sin);
             int r = recvfrom(sock, buf, 1024, 0, (struct sockaddr *)&sin, &slen);
+            if (verbose) {
+                fprintf(stderr, "recvfrom %d\n", r);
+            }
             if (r == 4 && !strncmp(buf, "here", 4)) {
                 fprintf(stdout, "%s\n", ipaddr(&sin));
+                if (once) {
+                    exit(0);
+                }
             }
             else if (r < 0) {
-                perror("recvfrom");
+                if (!quiet) {
+                    perror("recvfrom");
+                }
                 exit(2);
             }
             else {
-                fprintf(stderr, "unknown response from %s\n", ipaddr(&sin));
+                if (!quiet) {
+                    fprintf(stderr, "unknown response from %s\n", ipaddr(&sin));
+                }
             }
         }
     }
+    close(sock);
+    sock = -1;
     goto again;
-	return 0;
 }
 
